8ddf55aac9b5480f6d1d875368aa594f15b1a6d8
Willian Almeida <willianac01@gmail.com>
2024-02-26 19:57:21 -0300

    feat: create app navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import Navbar from "@/components/navbar";
 import Link from "next/link";
 
 export default function Home() {
 
   return (
-    <main className="flex w-full h-screen items-center justify-center">
+		<>
+		<Navbar />
+    <main className="flex w-full items-center justify-center">
 			<Link href="/auth/signin">
 				<button className="p-3 bg-indigo-500">Sign In</button>
 			</Link>
     </main>
+		</>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+
+function Navbar() {
+	return (
+		<header className="w-full h-14 bg-amber-500">
+			<div className="w-full max-w-[1180px] mx-auto flex justify-between items-center h-full">
+				<h1 className="font-bold text-lg text-zinc-800">E-commerce</h1>
+				<nav>
+					<ul className="flex gap-x-2 items-center">
+						<li><Link href="/auth/signin" className="text-zinc-800 hover:text-black">Entrar</Link></li>
+						<li>
+							<Link href="/auth/register">
+								<Button variant="ghost">
+									<span className="text-base text-zinc-800">Criar conta</span>
+									<ChevronRightIcon className="ml-1"/>
+								</Button>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	)
+}
+
+export default Navbar;
