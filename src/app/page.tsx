0a11c3be6bc680defddcb943058233298e8babf6
Willian Almeida <willianac01@gmail.com>
2024-02-26 20:03:47 -0300

    fix: fix main content height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
 		<>
 		<Navbar />
-    <main className="flex w-full items-center justify-center">
+    <main className="flex w-full items-center justify-center min-h-[calc(100vh-56px)] bg-zinc-100">
 			<Link href="/auth/signin">
 				<button className="p-3 bg-indigo-500">Sign In</button>
 			</Link>
