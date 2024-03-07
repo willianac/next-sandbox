266bcf290b51b53c2e7c42beaa28b9bc9ed50f83
Willian Almeida <willianac01@gmail.com>
2024-03-07 18:48:41 -0300

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import Navbar from "@/components/navbar";
 import { ProductCardProps } from "@/components/product-card";
 import Products from "./products";
 
-type Product = ProductCardProps & {id: string}
-
 export default async function Home() {
+	// const res = await fetch("http://localhost:3000/api/products", {cache: "no-store"})
+	// const data = await res.json()
+	// console.log(data.products[0])
   return (
 		<>
 			<Navbar />
--- src/app/products.tsx
+++ src/app/products.tsx
@@ -4,31 +4,24 @@ import ProductCard, { ProductCardProps } from "@/components/product-card"
 import { useQuery } from "@tanstack/react-query"
 import Link from "next/link"
 import { Skeleton } from "@/components/ui/skeleton"
-import { Button } from "@/components/ui/button"
 
 type Product = ProductCardProps & {id: string}
 
 function Products() {
-	const { data, isLoading, isError, refetch } = useQuery<Product[]>({
+	const { data, isLoading, isError } = useQuery<Product[]>({
 		queryKey: ["products"],
 		queryFn: async () => {
 			const res = await fetch("http://localhost:3001/products", {cache: "no-store"})
 			await new Promise((res) => setTimeout(res, 2000))
-			console.log("pegou!")
 			return await res.json()
 		},
 		staleTime: 60 * 1000
 	}) 
 
-	function handle() {
-		refetch()
-	}
-
 	return (
 		<main className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full max-w-[1180px] mx-auto pt-14 gap-y-9">
-			<Button onClick={handle}>Refetch</Button>
 			{isLoading && 
-				Array.from({length: 8}).map((_, i) => {
+				Array.from({length: 100}).map((_, i) => {
 					return (
 						<Skeleton key={i} className="w-[224px] h-[384px]"/>
 					)
